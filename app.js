6ea35bf900752ba8c63c3b26f092c9c12801415e
Xianyi555 <xianyi555@gmail.com>
2017-12-07 02:01:20 -0800

    completed racing cars game

--- app.js
+++ app.js
@@ -1,56 +1,57 @@
 console.log("js is working!")
 	
-// (I am still figuing out how to reset!)
+// (get ready for js)
    $(document).ready(function(){
 	   console.log("fully loaded!")
 	
+//    (reset the game)
+    $(".reset").on("click", function(){
 
-	// $(document).reset(function(){
-	// 	let $(".snoopyPilot").css("marginLeft") === ("35px");
-
- //        let $(".snoopyDriver").css("marginLeft") === ("30px");
-	// })
-
+    	$(".snoopyPilot").css("margin-left" , "35px");
+    	$(".snoopyDriver").css("margin-left" , "30px");
+    	isstarted = false;
+    })
 
 
-	let isstarted = false;
+
+
+// (set a false value to prepare the start button to use)
+	var isstarted = false;
 
 	$(".toStart").on("click", function(){
-	    if (isstarted === false) {
+		isstarted = true
+		console.log("start")
+	});
 
-	    	// let Winner = [];
-	
-
-	      $(window).on("keypress", function(raceActionOne){
-		  	if (raceActionOne.keyCode == 100) {
-          		$(".snoopyPilot").animate({"margin-left": "+=50px"})};
-          		
-          	        if ($(".snoopyPilot").css("marginLeft") === ("1335px")){
+	    	// (playerOne can win by these functons)
+	      $(document).on("keydown", function(raceActionOne){
+			if (isstarted) {
+	      	console.log(raceActionOne.keyCode)
+		  	if (raceActionOne.keyCode == 68) {
+          		$(".snoopyPilot").animate({"margin-left": "+=130px"})};
+          	        if (parseInt($(".snoopyPilot").css("marginLeft")) >= 1300){
 
           	     	    alert("Game Over, SnoopyPilot Won!");
-          	     	    isstarted !== false;
-          	          }
-          	    
-          	}
-          	);
+          	     	    // return isstarted !== false;
+          	     	    isstarted = false
+          	     	    console.log(isstarted)
+          	   }}});
+	      // (playerTwo can win by these functions)
+	      $(window).on("keydown", function(raceActionTwo){
+	      	if (isstarted) {
+		  	if (raceActionTwo.keyCode == 190) {
+		  		console.log(parseInt($(".snoopyDriver").css("marginLeft")))
+          		$(".snoopyDriver").animate({"marginLeft": "+=130px"});
 
-	
-	      $(window).on("keypress", function(raceActionTwo){
-		  	if (raceActionTwo.keyCode == 46) {
-          		$(".snoopyDriver").animate({"marginLeft": "+=50px"})};
-
-          		    if ($(".snoopyDriver").css("marginLeft") === ("1330px")){
+          		    if (parseInt($(".snoopyDriver").css("marginLeft")) >= 1300){
 
           	     	    alert("Game Over, SnoopyDriver Won!");
-          	     	    isstarted !== false;
-          	          }
-          })
-	    }
+          	     	    // return isstarted !== false;
+          	     	    isstarted = false
+          	 }}}
+          	})
 
-	})
-
-})
-
+});
 	
 
 
